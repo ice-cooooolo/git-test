7b79aee3208b65d1ebdab1293f95419698a1de09
ice-cooooolo <1807024877@qq.com>
2020-01-06 21:06:35 +0800

    项目创建

--- src/main/java/com/example/demo/github/Test.java
+++ src/main/java/com/example/demo/github/Test.java
@@ -6,5 +6,6 @@ package com.example.demo.github;
 public class Test {
     public static void main(String[] args) {
         System.out.println("这里是主分支");
+        System.out.println("这里是test分支！");
     }
 }
