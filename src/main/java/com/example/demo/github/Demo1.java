c9a3eb73ffc5e80990e51bfc47a7cefc2af9ea20
ice-cooooolo <1807024877@qq.com>
2020-01-06 21:17:03 +0800

    to test

--- src/main/java/com/example/demo/github/Demo1.java
+++ src/main/java/com/example/demo/github/Demo1.java
@@ -5,6 +5,7 @@ package com.example.demo.github;
  */
 public class Demo1 {
     public static void main(String[] args) {
-
+        System.out.println("我新添加了一个类");
+        System.out.println("我在基础上准备合并test");
     }
 }
