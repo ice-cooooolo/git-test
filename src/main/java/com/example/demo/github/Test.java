bff63e93679510f975dc12954f1b0ee28269d5c6
ice-cooooolo <1807024877@qq.com>
2020-01-06 21:02:39 +0800

    项目创建

---
+++ src/main/java/com/example/demo/github/Test.java
@@ -0,0 +1,10 @@
+package com.example.demo.github;
+
+/**
+ * @author lijunpeng
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("这里是主分支");
+    }
+}
